83689e9431195cd7da32a182796ab6a9a9e2e8d2
Maksym Shokin <problaze7@gmail.com>
2022-08-17 10:47:24 +0300

    fib-with-memo

--- basicFib.ts
+++ basicFib.ts
@@ -19,5 +19,19 @@ const fib2 = (n: number): number => {
   return sum;
 };
 
+const fibWithMemo = (n: number, memo: Record<number, number>): number => {
+  let result: number;
+  if (memo[n]) {
+    return memo[n];
+  }
+  if (n === 0 || n === 1) {
+    result = 1;
+  } else {
+    result = fibWithMemo(n - 1, memo) + fibWithMemo(n - 2, memo);
+  }
+  memo[n] = result;
+  return result;
+};
+
 console.log(fib(5));
 console.log(fib2(5));
